fa615549e87c46caed721d1208dd5343fc1bc166
Leena <leena.sn@multunus.com>
2011-11-04 20:55:19 +0530

    commiting the initial app

---
+++ feed_reader/src/org/feedreader/ListArticles.java
@@ -0,0 +1,15 @@
+package org.feedreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListArticles extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ feed_reader/src/org/feedreader/RSSParser.java
@@ -0,0 +1,13 @@
+package org.feedreader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RSSParser {
+
+	public List parseRSSFeed(String feedURL) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Object>();
+	}
+
+}
