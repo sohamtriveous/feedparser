e446e3de1e22160b3a0bc33d0f44477c6da1f27f
vaidy <vaidyanathan.b@multunus.com>
2011-11-05 19:22:48 +0530

    Fixed Bug: Special Characters such as ampersand chop off the titles

--- feed_reader/src/org/feedreader/RSSParser.java
+++ feed_reader/src/org/feedreader/RSSParser.java
@@ -1,9 +1,11 @@
 package org.feedreader;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -11,26 +13,35 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.CharacterData;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import android.sax.Element;
+import android.sax.EndElementListener;
+import android.sax.EndTextElementListener;
+import android.sax.RootElement;
+import android.util.Xml;
+
 public class RSSParser {
 
+	private static final String ROOT_ELEMENT = "rss";
 	private static final String TITLE = "title";
 	private static final String ITEM = "item";
 
-	public List getListOfTitles(URL feedURL) throws ParserConfigurationException, SAXException, IOException {
-		DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-		Document xmlDoc = builder.parse(feedURL.openStream());
-		NodeList itemNodes = xmlDoc.getElementsByTagName(ITEM);
-		List listOfItems = new ArrayList();
-		for (int i = 0; i < itemNodes.getLength(); i++) {
-			Element element = (Element)itemNodes.item(i);
-			CharacterData cData = (CharacterData) element.getElementsByTagName(TITLE).item(0).getFirstChild();
-			listOfItems.add(cData.getData());
-		}
+	public List getListOfTitles(URL feedURL)
+			throws ParserConfigurationException, SAXException, IOException {
+		final List listOfItems = new ArrayList();
+
+		RootElement root = new RootElement(ROOT_ELEMENT);
+		Element itemElement = root.getChild("channel").getChild(ITEM);
+		itemElement.getChild(TITLE).setEndTextElementListener(
+				new EndTextElementListener() {
+					@Override
+					public void end(String body) {
+						listOfItems.add(body);
+					}
+				});
+		Xml.parse(feedURL.openStream(), Xml.Encoding.UTF_8, root.getContentHandler());
 		return listOfItems;
-		
 	}
 }
--- feed_reader_test/src/org/feedreader/RSSParserTest.java
+++ feed_reader_test/src/org/feedreader/RSSParserTest.java
@@ -1,28 +1,20 @@
 package org.feedreader;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import junit.framework.TestCase;
 
 public class RSSParserTest extends TestCase {
 
 	private URL sampleURL;
-	private RSSParser parser;
+	private RSSParser parser = new RSSParser();
 	
 	public RSSParserTest () {
 	}
 	
 	public RSSParserTest(String name) {
 		super(name);
-		parser = new RSSParser();
 	}
 
 	protected void setUp() throws Exception {
@@ -38,18 +30,13 @@ public class RSSParserTest extends TestCase {
 		assertTrue("Did you pass a NULL URL?",  this.sampleURL != null);
 	}
 	
-	public void testListOfTitles() {
-		try {
+	public void testListOfTitles() throws Exception{
 			List listOfTitles = parser.getListOfTitles(this.sampleURL);
 			//System.out.println("lit of titles "+listOfTitles);
 			System.out.println("'" + listOfTitles.get(0) + "'");
-			assertEquals(listOfTitles.size(),10);
-			assertEquals(listOfTitles.get(0),"Wash your sins online at Saranam.com");
-			assertEquals(listOfTitles.get(9),"‘I unlock Joy’ program for developers &amp; students – Windows Phones up for grabs");
-		} catch (Exception e) {
-			fail("Exception thrown: " + e.getMessage());
-			e.printStackTrace();
-		}
+			assertEquals(10,listOfTitles.size());
+			assertEquals("Wash your sins online at Saranam.com", listOfTitles.get(0));
+			assertEquals("‘I unlock Joy’ program for developers & students – Windows Phones up for grabs", listOfTitles.get(9));
 	}
 	
 }
