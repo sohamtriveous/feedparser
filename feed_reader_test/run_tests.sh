c8919f4e3a55dad4698bca7f91b4a4b146dbbceb
vaidy <vaidyanathan.b@multunus.com>
2011-11-16 18:28:09 +0530

    changed the device id, added script for tagging the build

--- feed_reader_test/run_tests.sh
+++ feed_reader_test/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 curl http://localhost:9000/view/FeedParser/job/FeedParser/lastSuccessfulBuild/artifact/feed_reader/bin/FeedParser-debug.apk -O FeedParser-debug.apk
 curl http://localhost:9000/view/FeedParser/job/FeedParser/lastSuccessfulBuild/artifact/feed_reader_test/bin/FeedReaderTest-debug.apk -O FeedReaderTest-debug.apk
-/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s SH14NTJ11129 uninstall org.feedreader
-/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s SH14NTJ11129 uninstall org.feedreader.test
-/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s SH14NTJ11129 install -r FeedReaderTest-debug.apk
-/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s SH14NTJ11129 install -r FeedParser-debug.apk
-/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s SH14NTJ11129  shell am instrument -w org.feedreader.test/android.test.InstrumentationTestRunner
+/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s 32352CC496CC00EC uninstall org.feedreader
+/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s 32352CC496CC00EC uninstall org.feedreader.test
+/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s 32352CC496CC00EC install -r FeedReaderTest-debug.apk
+/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s 32352CC496CC00EC install -r FeedParser-debug.apk
+/Users/vaidy/Tools/android-sdk-macosx/platform-tools/adb -s 32352CC496CC00EC  shell am instrument -w org.feedreader.test/android.test.InstrumentationTestRunner
