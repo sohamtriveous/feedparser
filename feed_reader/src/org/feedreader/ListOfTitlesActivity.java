611748410ed97a3902b80f7446389e8172cde2e4
Ernest <ernest.sankeshwar@gmail.com>
2011-11-16 11:32:19 +0530

    Added Story View screen xml and some style changes

--- feed_reader/src/org/feedreader/ListOfTitlesActivity.java
+++ feed_reader/src/org/feedreader/ListOfTitlesActivity.java
@@ -20,6 +20,11 @@ public class ListOfTitlesActivity extends ListActivity {
 			exception.printStackTrace();
 		} 	
 		setContentView(R.layout.main);
+		
+//		WebView web = (WebView) findViewById(R.id.storyDescription);
+//		String string = "<div>      <a href='http://yourstory.in/2011/11/spruce-up-your-e-image-with-xtreme-digital-solutions/' title='Spruce up your e-image with Xtreme Digital Solutions'>                <img alt='Spruce up your e-image with Xtreme Digital Solutions' height='120' src='http://yourstory.in/wp-content/themes/NewStory2/scripts/timthumb.php?src=http://yourstory.in/wp-content/uploads/2011/11/xtreme_digital.jpg&amp;h=120&amp;w=150&amp;zc=1' width='150' />            </a>        </div><div id='spacetitle'>            <h2 class='title'>                <a href='http://yourstory.in/2011/11/spruce-up-your-e-image-with-xtreme-digital-solutions/' title='Spruce up your e-image with Xtreme Digital Solutions'>Spruce up your e-image with Xtreme Digital Solutions</a>            </h2>        </div><p class='pmar'>Spiderman can sure vouch for how important the web is to business. Evidently, any business can have a website, but if it is lost on the www, then maybe there is no point having one. So, to help your identity be unique online, Delhi-based Xtreme Digital Solutions, an online communication consultancy, crafts strategic content plans, aligning them with the business objectives of their clients.In conversation with Abhilasha Dafria for YourStory.in, Mohit Kukreja, co-founder of XDS tells more about how his e-venture offers their clients  a convenient, complete package of a high quality business...</p>" ;
+//		web.loadData(string, "text/html", "UTF-8");
+		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row,(String[])(titles.toArray(new String[0])));
 		this.setListAdapter(adapter);
 	}
