49f380d891bdc0b63f67417bbf61ce0fd54a75da
Ernest <ernest.sankeshwar@gmail.com>
2011-11-17 11:00:05 +0530

    added activity for showing details

---
+++ feed_reader/src/org/feedreader/ShowFeedDetailsActivity.java
@@ -0,0 +1,13 @@
+package org.feedreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShowFeedDetailsActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.story_view);
+		
+	}
+}
