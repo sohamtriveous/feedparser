6c9288a18708d79b194d7d42d7c62290c2104c07
Leena <leena.sn@multunus.com>
2011-11-22 15:38:20 +0530

    removed info, changed the appname to YourStory.in

--- feed_reader/src/org/feedreader/ShowFeedDetailsActivity.java
+++ feed_reader/src/org/feedreader/ShowFeedDetailsActivity.java
@@ -17,30 +17,33 @@ public class ShowFeedDetailsActivity extends Activity {
 		Intent intent = this.getIntent();
 		final String title = intent.getStringExtra("title");
 		final String url = intent.getStringExtra("contentURL");
-//		new ShortenURL(LOGIN, APIKEY)
+		// new ShortenURL(LOGIN, APIKEY)
 		WebView web = (WebView) findViewById(R.id.storyDescription);
-		TextView textView = (TextView) findViewById(R.id.storyTitle);		
+		TextView textView = (TextView) findViewById(R.id.storyTitle);
 		String content = intent.getStringExtra("content");
 		Log.d("Details activity", "content" + content);
 		web.loadDataWithBaseURL(url, content, "text/html", "utf-8", "");
 		textView.setText(title);
-		Button shareButton = (Button)findViewById(R.id.shareButton);
-		shareButton.setOnClickListener(new View.OnClickListener() {			
+		Button shareButton = (Button) findViewById(R.id.shareButton);
+		shareButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent(Intent.ACTION_SEND);
 				intent.setType(getString(R.string.share_message_type));
-				intent.putExtra(android.content.Intent.EXTRA_SUBJECT, title );
+				intent.putExtra(android.content.Intent.EXTRA_SUBJECT, title);
 				String shortenedURL = url;
-				try{
-					shortenedURL = new ShortenURL(getString(R.string.bitly_login),getString(R.string.bitly_api_key)).getShortUrl(url);
-				}
-				catch(Exception ex){
-					Log.w("", "Exception while shortening the URL "+ex);
+				try {
+					shortenedURL = new ShortenURL(
+							getString(R.string.bitly_login),
+							getString(R.string.bitly_api_key)).getShortUrl(url);
+				} catch (Exception ex) {
+					Log.w("", "Exception while shortening the URL " + ex);
 					ex.printStackTrace();
 				}
-				intent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.share_message)+" "+shortenedURL);	
-				startActivity(Intent.createChooser(intent, getString(R.string.share_button_label)));
+				intent.putExtra(android.content.Intent.EXTRA_TEXT,
+						getString(R.string.share_message) + " " + shortenedURL);
+				startActivity(Intent.createChooser(intent,
+						getString(R.string.share_button_label)));
 			}
 		});
 	}
