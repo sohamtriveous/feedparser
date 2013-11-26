ddcc2c26d9ed909702778cfbd85485beeef93f1f
KP <krishnaprasad.r@multunus.com>
2013-11-26 18:17:53 +0530

    add test for list view click

--- feed_reader/test/org/feedreader/ListOfTitlesActivityTest.java
+++ feed_reader/test/org/feedreader/ListOfTitlesActivityTest.java
@@ -1,15 +1,23 @@
 package org.feedreader;
 
 import static org.junit.Assert.assertEquals;
+import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.shadows.ShadowTextView;
 import org.robolectric.util.ActivityController;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 @RunWith(RobolectricTestRunner.class)
 public class ListOfTitlesActivityTest {
@@ -44,11 +52,20 @@ public class ListOfTitlesActivityTest {
 
 	@Test
 	public void onClickOnListViewItem_shouldShowTheItemDetails() {
-		// activityController.create();
-		// ListActivity activity = (ListActivity)activityController.get();
-		// View view = activity.getListView().getChildAt(0);
-		// view.callOnClick();
-		// activity.findViewById(id)
+		 Robolectric.addPendingHttpResponse(200, EXPECTED_XML_RESPONSE);
+		  activityController.create();
+		  
+		  ListActivity activity = (ListActivity)activityController.get();
+		  ListView view = activity.getListView();
+		  view.layout(0, 0, 100, 1000);
+		  view.performItemClick(view.getChildAt(0), 0, 0);
+		  
+		  ShadowActivity shadowActiviy = Robolectric.shadowOf(activity);
+		  Intent startedIntent = shadowActiviy.getNextStartedActivity();
+		  ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
+		  Assert.assertEquals(shadowIntent.getComponent().getClassName(), ShowFeedDetailsActivity.class.getName());
+		  
+		  
 
 	}
 
